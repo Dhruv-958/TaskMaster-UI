9b30d70bf64e4d1bbf79782276b40ce62ffb72f8
Khilesh Thakur <omthakur6640@gmail.com>
2025-05-01 00:52:33 +0530

    dash auth link

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div>
       this is the Home page.
+      <Link to="/dashboard">Go to Dashboard</Link>
+      <Link to="/auth">Go to Auth</Link>
     </div>
   )
 }
